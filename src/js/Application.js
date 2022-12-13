deb961a1ae423143559856aca516389f404e29b5
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-13 23:29:18 +0200

    add new code

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,16 +10,35 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
+    this._beat = new Beat();
+    this._beat.on('listen', function (data) {
+    this.emit(Beat.events.BIT);
 
-    document.querySelector(".main").appendChild(message);
+      if (count == 6) {
+        count = 0;
+      }
+  
+      let a = document.createElement('div');
+      a.classList.add('message');
+  
+      for (let i = 0; i < 1; i++) {
+        const element = lyrics[count];
+        document.querySelector(".main").appendChild(a);
+        a.innerHTML = element;
+        count++;
+      }
+  
+    })
+
+    this._create()
 
     this.emit(Application.events.READY);
+  }
+
+  _create() {
+
   }
 }
